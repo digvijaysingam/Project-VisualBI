babc92b4fed4123843d095a606bb92993335d282
Digvijay Singam (WT01 - Product Engineering Service) <DI324239@wipro.com>
2015-12-01 18:01:10 +0530

    First push

---
+++ js/measures.js
@@ -0,0 +1,13 @@
+(function(){
+  $.getJSON( "measures.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+})();
